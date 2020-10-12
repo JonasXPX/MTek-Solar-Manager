7ba422b63570ee4581914231de35faa0979d5699
JonasXPX <jonasdefarias@hotmail.com>
2020-10-12 13:51:18 -0300

    Inicio do projeto, configurações e dependencias

---
+++ src/main/java/br/uniamerica/mtecksolar/MTeck/Solar/MTeckSolarApplication.java
@@ -0,0 +1,13 @@
+package br.uniamerica.mtecksolar.MTeck.Solar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MTeckSolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MTeckSolarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/uniamerica/mtecksolar/MTeck/Solar/MTeckSolarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.uniamerica.mtecksolar.MTeck.Solar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MTeckSolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
